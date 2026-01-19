e6344eebb027bf5aa4d420a1e211a6b6e52ee444
mariannemassiani-96 <marianne.massiani@gmail.com>
2026-01-19 18:30:29 +0100

    Update SavorHome.tsx

--- components/home/SavorHome.tsx
+++ components/home/SavorHome.tsx
@@ -1,23 +1,11 @@
-"use client";
-
-import HomeHero from "@/components/home/sections/HomeHero";
-import StoryScrollGsap from "@/components/home/sections/StoryScrollGsap";
-import CalloutCurveMedia from "@/components/home/sections/CalloutCurveMedia";
-import FloatingMedia from "@/components/home/sections/FloatingMedia";
-import HomeRail from "@/components/home/sections/HomeRail";
-import PinnedSteps from "@/components/home/sections/PinnedSteps";
-import ShrinkTransition from "@/components/home/sections/ShrinkTransition";
-import AperturaOutro from "@/components/home/sections/AperturaOutro";
+import StoryCalloutCurve from "@/components/home/sections/StoryCalloutCurve";
 
 export default function SavorHome() {
   return (
     <div className="bg-graphite text-ivoire">
       <HomeHero />
-      <StoryScrollGsap />
-
-      <CalloutCurveMedia imageSrc="/media/home/02.jpg" />
-      <FloatingMedia />
-
+      <StoryCalloutCurve />
+      {/* puis la suite */}
       <HomeRail />
       <PinnedSteps />
       <ShrinkTransition />
