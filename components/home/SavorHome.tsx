401ed21b3cf0087ff2fc75b398bcf5e91b57dc17
mariannemassiani-96 <marianne.massiani@gmail.com>
2026-01-19 16:23:51 +0100

    Update SavorHome.tsx

--- components/home/SavorHome.tsx
+++ components/home/SavorHome.tsx
@@ -1,31 +1,14 @@
 "use client";
 
 import HomeHero from "@/components/home/sections/HomeHero";
-import StoryPanels from "@/components/home/sections/StoryPanels";
-import ShrinkTransition from "@/components/home/sections/ShrinkTransition";
-import FloatingImages from "@/components/home/sections/FloatingImages";
-import HomeRail from "@/components/home/sections/HomeRail";
+import StoryScroll from "@/components/home/sections/StoryScroll";
 import AperturaOutro from "@/components/home/sections/AperturaOutro";
 
 export default function SavorHome() {
   return (
     <main className="bg-graphite text-ivoire">
-      {/* 1) HERO fullscreen (vertical, savor-like) */}
       <HomeHero />
-
-      {/* 2) STORY fullscreen (4 tableaux) */}
-      <StoryPanels />
-
-      {/* 3) SHRINK transition */}
-      <ShrinkTransition />
-
-      {/* 4) FLOATING images (desktop), stack mobile */}
-      <FloatingImages />
-
-      {/* 5) (Option) Rail = “grappe / galerie” dans le flux */}
-      <HomeRail />
-
-      {/* 6) OUTRO APERTURA */}
+      <StoryScroll />
       <AperturaOutro />
     </main>
   );
