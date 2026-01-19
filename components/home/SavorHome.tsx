2118bc0d9be5d4480ac08d11efc7001cb0bab899
mariannemassiani-96 <marianne.massiani@gmail.com>
2026-01-19 18:22:51 +0100

    Update SavorHome.tsx

--- components/home/SavorHome.tsx
+++ components/home/SavorHome.tsx
@@ -2,6 +2,8 @@
 
 import HomeHero from "@/components/home/sections/HomeHero";
 import StoryScrollGsap from "@/components/home/sections/StoryScrollGsap";
+import CalloutCurveMedia from "@/components/home/sections/CalloutCurveMedia";
+import FloatingMedia from "@/components/home/sections/FloatingMedia";
 import HomeRail from "@/components/home/sections/HomeRail";
 import PinnedSteps from "@/components/home/sections/PinnedSteps";
 import ShrinkTransition from "@/components/home/sections/ShrinkTransition";
@@ -12,7 +14,16 @@ export default function SavorHome() {
     <div className="bg-graphite text-ivoire">
       <HomeHero />
       <StoryScrollGsap />
+
+      {/* Savor callout (media plein écran + courbe + texte qui glisse) */}
+      <CalloutCurveMedia mediaType="image" imageSrc="/media/home/02.jpg" />
+
+      {/* Images flottantes (3) */}
+      <FloatingMedia />
+
+      {/* Respiration galerie */}
       <HomeRail />
+
       <PinnedSteps />
       <ShrinkTransition />
       <AperturaOutro />
