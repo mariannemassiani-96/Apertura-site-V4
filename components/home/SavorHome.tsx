ad8b4053142efd4b50ce2a0b0dbd2cab352ea8b2
mariannemassiani-96 <marianne.massiani@gmail.com>
2026-01-19 18:38:40 +0100

    Update SavorHome.tsx

--- components/home/SavorHome.tsx
+++ components/home/SavorHome.tsx
@@ -1,11 +1,17 @@
+"use client";
+
+import HomeHero from "@/components/home/sections/HomeHero";
 import StoryCalloutCurve from "@/components/home/sections/StoryCalloutCurve";
+import HomeRail from "@/components/home/sections/HomeRail";
+import PinnedSteps from "@/components/home/sections/PinnedSteps";
+import ShrinkTransition from "@/components/home/sections/ShrinkTransition";
+import AperturaOutro from "@/components/home/sections/AperturaOutro";
 
 export default function SavorHome() {
   return (
     <div className="bg-graphite text-ivoire">
       <HomeHero />
       <StoryCalloutCurve />
-      {/* puis la suite */}
       <HomeRail />
       <PinnedSteps />
       <ShrinkTransition />
