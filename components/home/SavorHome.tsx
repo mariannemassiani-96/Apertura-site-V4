5235dc13c87e10e9f98a331616c43e698281cd73
mariannemassiani-96 <marianne.massiani@gmail.com>
2026-01-19 17:07:47 +0100

    Update SavorHome.tsx

--- components/home/SavorHome.tsx
+++ components/home/SavorHome.tsx
@@ -2,14 +2,12 @@
 
 import HomeHero from "@/components/home/sections/HomeHero";
 import StoryScrollGsap from "@/components/home/sections/StoryScrollGsap";
-import AperturaOutro from "@/components/home/sections/AperturaOutro";
 
 export default function SavorHome() {
   return (
     <div className="bg-graphite text-ivoire">
       <HomeHero />
       <StoryScrollGsap />
-      <AperturaOutro />
     </div>
   );
 }
